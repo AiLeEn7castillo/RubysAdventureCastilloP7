649116945527b4b5e80c1a96e166d791b6cbbd12
AiLeEn7castillo <aileen07castillo@gmail.com>
2023-11-17 15:23:31 -0800

    Bot
    I have added my bot and am starting to make the animations for him.

--- RubysAdventureCastilloPeriod7/Assets/Scripts/RubyController.cs
+++ RubysAdventureCastilloPeriod7/Assets/Scripts/RubyController.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class RubyController : MonoBehaviour
 {
     public float speed = 3.0f;
-    public int maxHealth = 5;
-    public int currentHealth;
 
+    public int maxHealth = 5;
+    public float timeInvincible = 2;
+    public int health { get { return currentHealth; } }
+    int currentHealth;
+
+    bool isInvincible;
+    float invincibleTimer;
 
     Rigidbody2D rigidbody2d;
     float horizontal;
@@ -16,9 +21,10 @@ public class RubyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-     rigidbody2d = GetComponent<Rigidbody2D>();
+        rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
-        currentHealth = 1;
+
+
     }
 
     // Update is called once per frame
@@ -26,6 +32,15 @@ public class RubyController : MonoBehaviour
     {
          horizontal = Input.GetAxis("Horizontal");
          vertical = Input.GetAxis("Vertical");
+
+        if (isInvincible)
+        {
+            invincibleTimer = Time.deltaTime;
+            if (invincibleTimer < 0)
+            {
+                isInvincible = false;
+            }
+        }
     }
     void FixedUpdate()
     { 
@@ -39,6 +54,15 @@ public class RubyController : MonoBehaviour
     }
     public  void ChangeHealth(int amount)
     {
+        if (amount < 0)
+        {
+            if (isInvincible)
+            {
+                return;
+            }
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
